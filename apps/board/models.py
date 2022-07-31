7d2196ffedda4cf1f2b72bd0e2ad11044169e4d1
Jason Silva <jasoon.rosa@gmail.com>
2022-07-31 14:59:57 -0300

    create models board/property/user

--- apps/board/models.py
+++ apps/board/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+from random import (
+    randint,
+    shuffle)
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from apps.user.models import (
+    Player,
+    IMPULSIVE,
+    EXIGENT,
+    CAUTIOUS,
+    RANDOM)
+from apps.property.models import Propertie
+
+
+def roll_dice():
+    return randint(1, 6)
+
+
+@dataclass
+class Gamer:
+    player: int
+    current_position: int = 0
+    amount: float = 300.0
+    shifts: int = 0
+
+
+class Board(object):
+    default_gamers = [
+        Gamer(Player(behavior=IMPULSIVE)),
+        Gamer(Player(behavior=EXIGENT)),
+        Gamer(Player(behavior=CAUTIOUS)),
+        Gamer(Player(behavior=RANDOM)),
+    ]
+    qty_properties: int
+    properties = List[Propertie]
+    gamers = List[Gamer]
+    excludes = []
+    players = List[Tuple]
+    result = List[tuple]
+    timeout = 0
+    inner = None
--- apps/property/models.py
+++ apps/property/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from dataclasses import dataclass
+from apps.user.models import Player
+import random
 
-# Create your models here.
+
+@dataclass
+class Propertie:
+    player: Player = None
+    sale_price: float = 0.0
+    rent_value: float = 0.0
+
+    @classmethod
+    def create(cls, min_value: float = 300, max_value: float = 30000.0):
+        """Create new 'Property' random values.
+        Args:
+            :param min_value: Minimum amount for 'sale_price'
+            :param max_value: Maximun amount for 'sale_price'
+            :param rent_value: Percentage of 'rent_value' based on 'sale_price'
+        Examples:
+            >>> propertie = Property.create()
+            >>> print(propertie)
+            Propertie(owner=None, sale_price=60.01903019134575, rent_value=6.001903019134574)
+        """
+        assert min_value < max_value  # TODO: handle exception
+        sale_price = random.uniform(min_value, max_value)
+        rent_value = random.uniform(1, 100)
+        return cls(sale_price=sale_price, rent_value=rent_value)
+
+
+if __name__ == '__main__':
+    propertie = Propertie.create()
+    print(propertie)
