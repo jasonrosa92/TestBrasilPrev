58dcf6e05440d71c393a0d2b10e806083227154c
Jason Silva <jasoon.rosa@gmail.com>
2022-07-31 15:27:56 -0300

    fix buggings in board/property

--- apps/property/models.py
+++ apps/property/models.py
@@ -4,23 +4,13 @@ import random
 
 
 @dataclass
-class Propertie:
+class Property:
     player: Player = None
     sale_price: float = 0.0
     rent_value: float = 0.0
 
     @classmethod
     def create(cls, min_value: float = 300, max_value: float = 30000.0):
-        """Create new 'Property' random values.
-        Args:
-            :param min_value: Minimum amount for 'sale_price'
-            :param max_value: Maximun amount for 'sale_price'
-            :param rent_value: Percentage of 'rent_value' based on 'sale_price'
-        Examples:
-            >>> propertie = Property.create()
-            >>> print(propertie)
-            Propertie(owner=None, sale_price=60.01903019134575, rent_value=6.001903019134574)
-        """
         assert min_value < max_value  # TODO: handle exception
         sale_price = random.uniform(min_value, max_value)
         rent_value = random.uniform(1, 100)
@@ -28,5 +18,6 @@ class Propertie:
 
 
 if __name__ == '__main__':
-    propertie = Propertie.create()
-    print(propertie)
+    property = Property.create()
+    print(property)
+    
