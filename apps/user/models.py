2a4579cefe5f2f20224b8a26f83a154a94ad9a37
Jason Silva <jasoon.rosa@gmail.com>
2022-07-31 12:23:50 -0300

    create apps

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from dataclasses import dataclass, field
+
+IMPULSIVE, EXIGENT, CAUTIOUS, RANDOM = range(4)
+BEHAVIOR_NAMES = {
+    IMPULSIVE: 'Impulsivo',
+    EXIGENT: 'Exigente',
+    CAUTIOUS: 'Cauteloso',
+    RANDOM: 'Aleatório'
+}
+
+
+@dataclass
+class Player:
+    behavior: int
+
+    @property
+    def title(self):
+        return BEHAVIOR_NAMES.get(self.behavior)
+
+
+if __name__ == '__main__':
+    player = Player(0)
+    print(player)
+    print(player.title)
